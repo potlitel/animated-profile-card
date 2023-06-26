7154bccf36411dfb0f954564e937a959061d6202
potlitel <potlitel@gmail.com>
2023-06-26 08:24:57 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+https://codepen.io/gulam-anas/pen/eYQBPjj
+https://github.com/FortAwesome/angular-fontawesome
+https://stackoverflow.com/questions/61447049/angular-fontawesome-dependencies-missing-error
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
